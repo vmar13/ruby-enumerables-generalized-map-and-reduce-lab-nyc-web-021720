bcd70fc3d1b7c1c11c122d92d455b4c3eb68759b
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 21:43:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,11 @@
-# def map(source_array)
-#   new_array = []
+def map(array)
+  new_array = []
+  counter = 0 
+  while counter < array.length 
+    new_array.push(yield(array[counter]))
+    i +=
+  end 
+  new_array  
+end 
   
   
